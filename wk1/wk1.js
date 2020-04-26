bfea0ae357ecb398c1d00ff7280bb0d321c0ff9f
MitchellRenee69 <arcasia@netzero.net>
2020-04-25 23:28:46 -0700

    Add wk1 files

--- wk1/wk1.js
+++ wk1/wk1.js
@@ -1,1 +1,17 @@
 //LOCAL STORAGE EX//
+/*How to use getItem()*/
+window.localStorage.getItem('user');
+/*Using removeItem()*/
+window.localStorage.removeItem('name');
+/*Using the key()method*/
+var KeyName = window.localStorage.key(index);
+/*Retrieving data from Windows storage*/
+// Check browser support
+if (typeof(Storage) !== "undefined") {
+    // Store
+    localStorage.setItem("lastname", "Smith");
+    // Retrieve
+    document.getElementById("result").innerHTML = localStorage.getItem("lastname");
+  } else {
+    document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+  }
