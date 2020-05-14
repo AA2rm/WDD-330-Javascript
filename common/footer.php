9078266ef0c38e743239249cf5fc2f5ff5095a0f
MitchellRenee69 <arcasia@netzero.net>
2020-05-14 16:40:53 -0700

    Wk 4.0 Assignments

---
+++ common/footer.php
@@ -0,0 +1,7 @@
+<!--write code to get rid of list dots on error page-->
+<li><a href=”#” title=”Learn how JS, CSS, HTML, PHP build web pages>About Programming Lanugages</a>
+</li>
+
+ <a href=”#” title=”Learn front-end development with me”>About Front-end Development</a>
+</ul>
+<br><br>
---
+++ common/header.php
@@ -0,0 +1,3 @@
+<a href='/WDD 340/common/header.php' title="Visit home page"> <img class="mylogo" src="/WDD 340/images/sites/heartslogo.jpg"  alt="Hearts Logo"></a>
+
+<a href='/WDD 340/common/header.php' title="Visit home page"> <img class="avartar" src="/WDD 340/images/sites/avartar.jpg"  alt="Lady avatar"></a>
