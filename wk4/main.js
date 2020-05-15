a56a92544b458626dc89186a53570f4498f4b2d4
MitchellRenee69 <arcasia@netzero.net>
2020-05-14 17:59:39 -0700

    Wk 4.1 Assignments

--- wk4/main.js
+++ wk4/main.js
@@ -1,2 +1,12 @@
+//Form Events
 const input = form.elements.searchInput;
 input.addEventListener('focus', () => alert('focused'), false);
+input.addEventListener('blur', () => alert('blurred'), false);
+input.addEventListener('change', () => alert('changed'), false);
+
+//Submit a form
+const form = document.forms['search'];
+form.addEventListener ('submit', search, false);
+function search() {
+    alert(' Form Submitted');
+}
